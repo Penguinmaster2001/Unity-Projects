c824dd03fcaf5ad1ce2344070fc75605b11cd7c9
Penguinmaster2001 <98120016+Penguinmaster2001@users.noreply.github.com>
2023-01-13 00:30:06 -0800

    Add files via upload

---
+++ procedural star system generator/CameraTargetScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTargetScript : MonoBehaviour
+{
+    //public Camera camera;
+    private Vector3 velocity;
+    //private Vector3 randPos;
+    private Vector3 targetPos = Vector3.zero;
+    private GameObject targetPlanet;
+    public GameObject[] planets;
+
+    void Update()
+    {
+        planets = GameObject.FindGameObjectsWithTag("Planet");
+
+        if (targetPlanet == null || Vector3.Distance(transform.position, targetPos) < targetPlanet.transform.lossyScale.x + 1)
+        {
+            targetPlanet = planets[Random.Range(0, planets.Length)];
+        }
+
+        targetPos = targetPlanet.transform.position;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, Vector3.Distance(transform.position, targetPos) / 1000);
+        transform.LookAt(targetPlanet.transform.position);
+    }
+}
---
+++ procedural star system generator/NameLists.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameLists : MonoBehaviour
+{
+    public static string[] alienNames   = new string[4]     {"Nortblac", "voschk", "rathms", "turgh"};
+
+    public static string[] tPlanetNames = new string[15]    { "Zentar", "Huner", "Endaenia", "Chelneturn", "Uchomia", "Itrippe", "Liupra", "Rania", "Bangutune", "Tundoria", "Udrora", "Getania", "Ocury", "Cevinus", "Goyuhiri"};
+
+    public static string[] gPlanetNames = new string[15]    { "Zentar", "Huner", "Endaenia", "Chelneturn", "Uchomia", "Itrippe", "Liupra", "Rania", "Bangutune", "Tundoria", "Udrora", "Getania", "Ocury", "Cevinus", "Goyuhiri" };
+
+    public Texture[] portraits;
+    public static Texture[] alienPortraits;
+
+    void Start()
+    {
+        alienPortraits = portraits;
+    }
+}
