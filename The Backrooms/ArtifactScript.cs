74e2dc2a2248094c4b02cfe87e8e47adc5e9e555
Penguinmaster2001 <98120016+Penguinmaster2001@users.noreply.github.com>
2023-01-13 00:53:48 -0800

    Create ArtifactScript.cs

---
+++ The Backrooms/ArtifactScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArtifactScript : MonoBehaviour
+{
+    void Destroy()
+    {
+        Destroy(gameObject);
+    }
+}
